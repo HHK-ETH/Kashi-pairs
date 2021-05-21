ce81a0cecd5b6d0d825a3c50db6637a8b9573d99
HHK <hhk.contact@protonmail.com>
2021-05-21 13:50:31 +0200

    feat: create empty pairs page component

---
+++ src/pages/pairs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {KashiPairInfos} from "../utils/constants";
+
+function Pairs({kashiPairs}: {kashiPairs: KashiPairInfos[]}): JSX.Element {
+    return (
+        <div>
+            pairs
+        </div>
+    );
+}
+
+export default Pairs;
