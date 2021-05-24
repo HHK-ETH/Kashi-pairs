fb34f0fca6b7906b4f6d2d5c3f6c31e9dbbf7546
HHK <hhk.contact@protonmail.com>
2021-05-24 17:58:45 +0200

    feat: display pairs

--- src/pages/pairs.tsx
+++ src/pages/pairs.tsx
@@ -6,26 +6,45 @@ function getTokenInfos(address: string): Token | undefined {
     return TOKEN_LIST.find(token => token.address === address);
 }
 
-function Pairs({kashiPairs}: {kashiPairs: KashiPairInfos[]}): JSX.Element {
+export function SearchPair({setTokenSymbol}: {setTokenSymbol: Function}): JSX.Element {
+    return (
+        <div className="text-center">
+            <label htmlFor="search" className="form-label">Search a token symbol</label>
+            <input type="text" className="form-control" id="search" placeholder={"Enter token symbol here"} onChange={(event => setTokenSymbol(event.target.value))}/>
+        </div>
+    );
+}
+
+function Pairs({kashiPairs, search}: {kashiPairs: KashiPairInfos[], search: string}): JSX.Element {
     return (
         <div>
             {
-                kashiPairs.map((pair, index) => {
+                kashiPairs.sort((pairA, pairB) => {
+                    if (parseInt(formatEther(pairA.totalAsset.elastic), 10) > parseInt(formatEther(pairB.totalAsset.elastic), 10)) {
+                        return -1
+                    }
+                    if (parseInt(formatEther(pairA.totalAsset.elastic), 10) < parseInt(formatEther(pairB.totalAsset.elastic), 10)) {
+                        return 1
+                    }
+                    return 0;
+                }).map((pair, index) => {
                     const asset: Token | undefined = getTokenInfos(pair.assetAddress);
                     const collateral: Token | undefined = getTokenInfos(pair.collateralAddress);
                     const totalAsset: number = parseInt(formatEther(pair.totalAsset.elastic), 10) + parseInt(formatEther(pair.totalBorrow.elastic), 10);
-                    if (asset !== undefined && collateral !== undefined && totalAsset > 0.01) {
+                    const kmValue: number = totalAsset / parseInt(formatEther(pair.totalAsset.base), 10)
+                    if (asset !== undefined && collateral !== undefined &&
+                        (asset.symbol.toLowerCase().indexOf(search.toLowerCase()) !==  -1 || collateral.symbol.toLowerCase().indexOf(search.toLowerCase()) !== -1)) {
                         return (
                         <div className="card text-center">
                             <div className="card-header">
-                                1 - km{collateral.symbol}-{asset.symbol} = x tokens
+                                1 - km{collateral.symbol}-{asset.symbol} = {kmValue ? kmValue : 1} {asset.symbol} = x $
                             </div>
                             <div className="card-body container">
                                 <img src={asset.logoURI} alt={"img"} className={"col-3"}/>
                                 <img src={collateral.logoURI} alt={"img"} className={"col-3"}/>
                             </div>
                             <div className="card-footer text-muted">
-                                Total asset: {totalAsset}
+                                Total asset: {totalAsset} {asset.symbol}
                             </div>
                         </div>
                         );
