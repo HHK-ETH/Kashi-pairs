912e1a8a2e96c25646515d670e623ba5d45af89f
HHK <hhk.contact@protonmail.com>
2021-05-24 15:12:21 +0200

    feat: upgrade route component

--- src/components/Route.tsx
+++ src/components/Route.tsx
@@ -1,5 +1,22 @@
+import {useEffect, useState} from "react";
+
 function Route({path, children}: {path: any, children: any}) {
-    return window.location.pathname === path ? children : null
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        // define callback as separate function so it can be removed later with cleanup function
+        const onLocationChange = () => {
+            setCurrentPath(window.location.pathname);
+        }
+        window.addEventListener('popstate', onLocationChange);
+        // clean up event listener
+        return () => {
+            window.removeEventListener('popstate', onLocationChange)
+        };
+    }, [])
+    return currentPath === path
+        ? children
+        : null;
 }
 
 export default Route;
