04ad855f916469870d2eff3bad7ceda67bba8853
HHK <hhk.contact@protonmail.com>
2021-05-24 17:58:32 +0200

    feat: add search component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import Route from "./components/Route";
-import Pairs from "./pages/pairs";
+import Pairs, {SearchPair} from "./pages/pairs";
 import Home from "./pages/home";
 import {KashiPairInfos} from "./utils/constants";
 
 function App() {
 
     const [kashiPairs, setKashiPairs]: [KashiPairInfos[], Function] = useState([]);
+    const [tokenSymbol, setTokenSymbol]: [string, Function] = useState('');
 
     return (
         <div className={'container'}>
@@ -14,7 +15,8 @@ function App() {
                 <Home setKashiPairs={setKashiPairs}/>
             </Route>
             <Route path={'/pairs'}>
-                <Pairs kashiPairs={kashiPairs}/>
+                <SearchPair setTokenSymbol={setTokenSymbol}/>
+                <Pairs kashiPairs={kashiPairs} search={tokenSymbol}/>
             </Route>
         </div>
     );
